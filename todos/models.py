51817dc04bda951028c23df63414e8aba4fca6eb
Patrick Allen <prallen90@gmail.com>
2015-07-14 18:10:00 +0000

    Add TodoList and TodoItem models

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class TodoList(models.Model):
+    title = models.CharField(max_length=120, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    public = models.BooleanField(default=False)
+    # owner = models.ForeignKey('User')
+
+
+class TodoItem(models.Model):
+    LOW = 'L'
+    MEDIUM = 'M'
+    HIGH = 'H'
+    PRIORITY_CHOICES = (
+        (LOW, 'Low'),
+        (MEDIUM, 'Medium'),
+        (HIGH, 'High'),
+    )
+    task = models.CharField(max_length=120, blank=False)
+    priority = models.CharField(max_length=2, choices=PRIORITY_CHOICES)
+    due_date = models.DateField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
