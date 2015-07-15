2ee884005c2dab4cce6cd0f3832fb0c1c85b1400
Patrick Allen <prallen90@gmail.com>
2015-07-14 22:02:51 -0400

    Set up postgresql as database

--- tominder/settings.py
+++ tominder/settings.py
@@ -74,20 +74,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tominder.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tominderdb',
+        'USER': 'username',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
