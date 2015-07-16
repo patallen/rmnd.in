f693d2ef2f279bf857aa8241c4abf73dcdbdbce8
Patrick Allen <prallen90@gmail.com>
2015-07-16 14:34:52 +0000

    Set default permissions to 'IsAuthenticated'

--- tominder/settings.py
+++ tominder/settings.py
@@ -85,6 +85,12 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
