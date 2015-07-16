84f2fc1776a35eb0def740e4ae3f9996ec4a1532
Patrick Allen <prallen90@gmail.com>
2015-07-16 12:40:23 -0400

    Set up index.html template for use with angular

--- tominder/settings.py
+++ tominder/settings.py
@@ -55,11 +55,13 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'tominder.urls'
-
+TEMPLATE_DIRS = [
+    os.path.join(BASE_DIR, 'templates')
+]
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': TEMPLATE_DIRS,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
