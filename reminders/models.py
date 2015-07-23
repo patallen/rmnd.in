df55f961bd50cff3e628ef3a7956bb2905598c62
Patrick Allen <prallen90@gmail.com>
2015-07-23 14:54:44 +0000

    Remove task in Reminder model

--- reminders/models.py
+++ reminders/models.py
@@ -4,8 +4,7 @@ from django.contrib.auth.models import User
 
 class Reminder(models.Model):
     title = models.CharField(max_length=120, blank=False)
-    task = models.CharField(max_length=240, blank=False)
-    notes = models.TextField()
+    notes = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     remind_date = models.DateTimeField()
