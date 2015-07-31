dd222445c3eb4dce32316e396df7be0822cb054a
Patrick Allen <prallen90@gmail.com>
2015-07-30 21:45:54 -0400

    Add logging for collect_reminders cron

--- reminders/cron.py
+++ reminders/cron.py
@@ -1,3 +1,20 @@
+from reminders.models import Reminder
+from django.contrib.auth.models import User
+from django.utils import timezone
+import os
+import logging
+
+cdir = os.path.dirname(os.path.realpath(__file__))
+cdirup = os.path.dirname(cdir)
+cfile = cdirup + '/collect.log'
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+loghandle = logging.FileHandler(cfile, mode="a")
+log.addHandler(loghandle)
 
 def collect_reminders():
-    print('YES')
+    rem = Reminder.objects.create(title='Cron',
+                                  remind_date=timezone.now(),
+                                  owner=User.objects.first())
+    log.debug(rem)
