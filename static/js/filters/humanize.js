03d85c9f95e0fab3e7365554161d3c409c480019
Patrick Allen <prallen90@gmail.com>
2015-08-06 11:14:56 -0400

    Alter humanize filter to user short times (ex: 1d 2h)

--- static/js/filters/humanize.js
+++ static/js/filters/humanize.js
@@ -21,32 +21,63 @@ app.filter('humanize', function(){
 		left = left % MIN;
 		var seconds = left % MIN;
 		
+		// if (days > 0){
+		// 	if (days === 1){
+		// 		return "1 day and " + hours + " hours";
+		// 	}
+		// 	else {
+		// 		return days + " days and " + hours + " hours";
+		// 	}
+		// }
+		// else if (hours > 0){
+		// 	if (hours === 1){
+		// 		return "1 hour and " + minutes + " min";
+		// 	}
+		// 	else {
+		// 		return hours + " hours " + minutes + " min";
+		// 	}
+		// }
+		// else if (minutes > 0){
+		// 	if (minutes === 1){
+		// 		return "1 min";
+		// 	}
+		// 	else {
+		// 		return minutes + " min";
+		// 	}
+		// }
+		// else if (minutes === 0){
+		// 	return "Less than a minute";
+		// }
+		// else {
+		// 	return "Past";
+		// }
+
 		if (days > 0){
 			if (days === 1){
-				return "1 day and " + hours + " hours";
+				return "1d " + hours + "h";
 			}
 			else {
-				return days + " days and " + hours + " hours";
+				return days + "d " + hours + "h";
 			}
 		}
 		else if (hours > 0){
 			if (hours === 1){
-				return "1 hour and " + minutes + " min";
+				return "1h " + minutes + "m";
 			}
 			else {
-				return hours + " hours " + minutes + " min";
+				return hours + "h " + minutes + "m";
 			}
 		}
 		else if (minutes > 0){
 			if (minutes === 1){
-				return "1 min";
+				return "1m";
 			}
 			else {
-				return minutes + " min";
+				return minutes + "m";
 			}
 		}
 		else if (minutes === 0){
-			return "Less than a minute";
+			return "< 1m";
 		}
 		else {
 			return "Past";
