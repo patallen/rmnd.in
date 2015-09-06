0eea8a54029ca21fff7668a7c051e1ed2b5a0aa2
Patrick Allen <prallen90@gmail.com>
2015-09-05 20:08:58 -0400

    Update gulpfile and index to inject to only one tag set

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,18 +1,25 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var bower = require('gulp-bower');
 var bowerFiles = require('main-bower-files');
-var inject = require('gulp-inject');
-var livereload = require('gulp-livereload');
-var es = require('event-stream');
-var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var debug = require('gulp-debug');
-var minifyCss = require('gulp-minify-css');
-var ngFileSort = require('gulp-angular-filesort');
 var del = require('del');
+var es = require('event-stream');
+var gulp = require('gulp');
+var inject = require('gulp-inject');
+var livereload = require('gulp-livereload');
+var minifyCss = require('gulp-minify-css');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
 
+var appStream = gulp.src(['./static/css/*.css', './static/js/**/*.js', '!../static/js/test/*']);
+var bowerStream = gulp.src(bowerFiles(), {read: false});
+var indexHTML = gulp.src('./templates/index.html');
+
+gulp.task('bower', function(){
+  return bower()
+    .pipe(gulp.dest('static/components'))
+});
 
 gulp.task('styles', function(){
   gulp.src('static/sass/**/*.sass')
@@ -21,74 +28,44 @@ gulp.task('styles', function(){
     .pipe(gulp.dest('static/css/'))
     .pipe(livereload());
 });
-gulp.task('bower', function(){
-  return bower()
-		.pipe(gulp.dest('static/components'))
-});
 
 gulp.task('watch', function(){
-	livereload.listen();
+  livereload.listen();
   gulp.watch('static/sass/**/*.sass', ['styles']);
 });
-gulp.task('bdev', ['bower', 'styles'], function(){
-  var target = gulp.src('./templates/index.html');
-  var custom = gulp.src(['./static/css/*.css', './static/js/**/*.js', '!./static/js/test/*'], {read: false});
-  var bowerfiles = gulp.src(bowerFiles(), {read: false});
-  return target.pipe(inject(bowerfiles, {name: 'bower'}))
-    .pipe(inject(es.merge(custom), {name: 'custom'}))
-    .pipe(gulp.dest('./templates'));
+
+gulp.task('build-dev',['bower', 'styles'], function(){
+  indexHTML
+	.pipe(inject(es.merge(bowerStream, appStream)))
+	.pipe(gulp.dest('./templates'));
 });
 
-gulp.task('bprod', ['bdev'], function(){
-  var bowerfiles = gulp.src(bowerFiles(), {read: false});
-  bowerfiles
-    .pipe(uglify())
-    .pipe(concat('libs.min.js'))
-    .pipe(gulp.dest('./static/dist/'));
-  var target = gulp.src('./templates/index.html');
-  // Minify and concatenate application JS scripts
-  gulp.src(['./static/js/**/*.js', '!./static/js/test/**/*.js'])
-    .pipe(uglify())
-    .pipe(concat('app.min.js'))
-    .pipe(gulp.dest('./static/dist/'));
-
-  return target.
-    pipe(inject(gulp.src('./static/dist/*'), {name: 'custom'})).
-    pipe(gulp.dest('./templates'));
-
-});
-gulp.task('default', ['bdev', 'styles', 'watch'], function(){
-   // Perform bower and styles then watch server 
-});
-
-gulp.task('prep-release', ['styles'], function(){
+gulp.task('build-release', ['build-dev'], function(){
   del('./static/dist');
-  gulp.src(bowerFiles('**/*.js')).pipe(uglify())
+  var bowerJsStream = gulp.src(bowerFiles('**/*.js'))
+	.pipe(uglify())
     .pipe(concat('libs.min.js'))
     .pipe(gulp.dest('./static/dist/js/'));
 
-  gulp.src(bowerFiles('**/*.css'))
+  var bowerCssStream = gulp.src(bowerFiles('**/*.css'))
     .pipe(minifyCss())
     .pipe(concat('libs.min.css'))
     .pipe(gulp.dest('./static/dist/css/'));
 
-  gulp.src('./static/css/*.css')
+  var appCssStream = gulp.src('./static/css/*.css')
     .pipe(minifyCss())
     .pipe(concat('styles.min.css'))
     .pipe(gulp.dest('./static/dist/css/'));
-  
-  return gulp.src(['./static/js/**/*.js', '!./static/js/test/**/*.js'])
-		.pipe(uglify())
-		.pipe(concat('app.min.js'))
-		.pipe(gulp.dest('./static/dist/js'));
+
+  var appJsStream = gulp.src(['./static/js/**/*.js', '!./static/js/test/**/*.js'])
+	.pipe(uglify())
+	.pipe(concat('app.min.js'))
+	.pipe(gulp.dest('./static/dist/js'));
+	indexHTML
+      .pipe(inject(es.merge(bowerJsStream, appJsStream, bowerCssStream, appCssStream)))
+      .pipe(gulp.dest('./templates'));
 });
 
-gulp.task('build-release', ['prep-release'], function(){
-  var target = gulp.src('./templates/index.html');
-  var cssSource = gulp.src(['./static/dist/css/*.min.css']);
-  var jsSource = gulp.src(['./static/dist/js/*.min.js']).pipe(ngFileSort());
-  return target
-    .pipe(inject(cssSource, {name: 'custom'}, {read: false}))
-    .pipe(inject(jsSource, {name: 'custom'}))
-    .pipe(gulp.dest('./templates'));
-})
+gulp.task('default', ['build-dev', 'styles', 'watch'], function(){
+   // Perform bower and styles then watch server
+});
