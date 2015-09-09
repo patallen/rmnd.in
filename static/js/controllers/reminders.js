33d7772b323f1584221633d323aeee7de11bbfdb
Patrick Allen <prallen90@gmail.com>
2015-09-09 18:39:38 +0000

    Refactor Reminder controller

--- static/js/controllers/reminders.js
+++ static/js/controllers/reminders.js
@@ -10,29 +10,51 @@
 	function Reminder($interval, ReminderService) {
     var vm = this;
 		var _menuActive = null;
-		vm.setMenuActive = _setMenuActive;
-		vm.getMenuActive = _getMenuActive;
 
-		vm.getStatusClass = _getStatusClass;
-		vm.getPauseButtonClass = _getPauseButtonClass;
-
-		vm.toggleHold = _toggleHold;
-
-		vm.sortType = ['-complete', '-created_at'];
+    // Controller Variables
+    vm.sortType = ['-complete', '-created_at'];
 		vm.sortReverse = true;
 		vm.filterStr = 'all';
 
-		function _setMenuActive(reminder){
-			if (reminder == _menuActive){
-				_menuActive = null;
-			} else {
-				_menuActive = reminder;
-			}
+    // Set Reminders
+    // TODO: Do in resolve?
+    vm.reminders = ReminderService.reminders;
+
+    vm.deleteReminder =  deleteReminder;
+    vm.isActiveFilter =  isActiveFilter;
+    vm.getMenuActive = getMenuActive;
+		vm.getPauseButtonClass = getPauseButtonClass;
+    vm.getPriority = getPriority;
+    vm.getStatusClass = getStatusClass;
+    vm.setFilter =  setFilter;
+		vm.setMenuActive = setMenuActive;
+    vm.toggleHold = toggleHold;
+
+
+    function deleteReminder(reminder) {
+			ReminderService.deleteReminder(reminder);
 		}
-		function _getMenuActive(){
+
+    function isActiveFilter(filter){
+			if(filter == vm.filterStr){
+				return true;
+			}
+			return false;
+		}
+
+    function getMenuActive(){
 			return _menuActive;
 		}
-		vm.getPriority = function(reminder){
+
+    function getPauseButtonClass(reminder) {
+			if (reminder.on_hold){
+				return "fa fa-play";
+			} else {
+				return "fa fa-pause";
+			}
+		}
+
+		function getPriority(reminder){
 			switch(reminder.priority){
 				case 'L':
 					return 'Low';
@@ -43,22 +65,9 @@
 				default:
 					return 'Low';
 			}
-		};
-		vm.setFilter = function (filter){
-			if (filter =='upcoming'){
-				vm.filterStr = 'upcoming';
-				vm.sortType = 'remind_date';
-			}
-			else if (filter=='sent'){
-				vm.filterStr = 'sent';
-			}
-			else{
-				vm.filterStr = 'all';
-				vm.sortType = ['-complete', '-created_at'];
-			}
-		};
+		}
 
-		function _getStatusClass(reminder) {
+    function getStatusClass(reminder) {
 			var now = new Date().getTime();
 			var rt = new Date(reminder.remind_date).getTime();
 			if (reminder.on_hold){
@@ -70,42 +79,29 @@
 			}
 		}
 
-		function _getPauseButtonClass(reminder) {
-			if (reminder.on_hold){
-				return "fa fa-play";
-			} else {
-				return "fa fa-pause";
+		function setFilter(filter){
+			if (filter =='upcoming'){
+				vm.filterStr = 'upcoming';
+				vm.sortType = 'remind_date';
+			}
+			else if (filter=='sent'){
+				vm.filterStr = 'sent';
+			}
+			else{
+				vm.filterStr = 'all';
+				vm.sortType = ['-complete', '-created_at'];
 			}
 		}
 
-		vm.isActiveFilter = function (filter){
-			if(filter == vm.filterStr){
-				return true;
+		function setMenuActive(reminder){
+			if (reminder == _menuActive){
+				_menuActive = null;
+			} else {
+				_menuActive = reminder;
 			}
-			return false;
-		};
-		vm.toggleReverse = function() {
-			vm.sortReverse = !vm.sortReverse;
-		};
+		}
 
-		vm.setSortType = function(sortType){
-			vm.sortType = sortType;
-		};
-	 vm.getDirectionClass = function(){
-			if(vm.sortReverse === true){
-				return 'glyphicon-arrow-up';
-			}else{
-				return 'glyphicon-arrow-down';
-			}
-		};
-
-		vm.reminders = ReminderService.reminders;
-
-		vm.delete = function(reminder) {
-			ReminderService.deleteReminder(reminder);
-		};
-
-		function _toggleHold(reminder){
+    function toggleHold(reminder){
 			var on_hold = reminder.on_hold;
 			var msg = '';
 
@@ -123,5 +119,22 @@
 					toastr.error("Could not toggle hold.");
 				});
 		}
+
+
+    // Currently Unused Functions
+		vm.toggleReverse = function() {
+			vm.sortReverse = !vm.sortReverse;
+		};
+
+		vm.setSortType = function(sortType){
+			vm.sortType = sortType;
+		};
+	  vm.getDirectionClass = function(){
+			if(vm.sortReverse === true){
+				return 'glyphicon-arrow-up';
+			}else{
+				return 'glyphicon-arrow-down';
+			}
+		};
 	}
 })();
